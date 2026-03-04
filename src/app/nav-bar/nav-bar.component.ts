c4747aa797ae19da22ecab5c32622942a3dddbf4
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2026-03-04 22:29:04 +0100

    Ajout des channels dans la navBar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
+import { NavBarService, Channel } from './nav-bar.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgFor,
+    NgIf
 ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  
+  // On initialise channel a un tableau vide et isLogged a false
+  channels: Channel[] = [];
+  isLogged = true;
+  
+  constructor(private navBarService: NavBarService) {}
+  
+  // Au chargement de la page on fait la requête
+  ngOnInit(): void {
+    this.navBarService.getChannel().subscribe({
+      next: (data) => {
+        this.channels = data;
+        this.isLogged;
+        console.log("Channels récupéres:", data);
+      },
+      error: (err) => {
+        console.log("Utilisateur non connecté");
+        this.isLogged = false;
+      }
+    });
+  }
 
 }
---
+++ src/app/nav-bar/nav-bar.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Channel {
+  id: number;
+  name: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class NavBarService {
+
+  constructor(private http: HttpClient) { }
+//On fait la requête pour récupérer les channels de l'utilisateur
+  getChannel(): Observable<Channel[]> {
+    return this.http.get<Channel[]>('http://localhost:8000/api/get_channels/', 
+      {withCredentials: true}
+    ) 
+    
+  }
+}
